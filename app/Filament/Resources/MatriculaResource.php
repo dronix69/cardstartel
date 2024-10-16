12348ff70e6e13bf929dbe4360adb9b8f5c2e0da
dronix <Daniel ferrada>
2024-10-16 12:36:53 -0300

    actualizacion de modulos

--- app/Filament/Resources/MatriculaResource.php
+++ app/Filament/Resources/MatriculaResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Filament\Resources;
 
+use App\Filament\Exports\MatriculaExporter;
 use App\Filament\Resources\MatriculaResource\Pages;
-use App\Filament\Resources\MatriculaResource\RelationManagers;
 use App\Models\Matricula;
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -12,18 +12,143 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Actions\ExportAction;
+use Filament\Tables\Actions\ExportBulkAction;
+use Filament\Tables\Filters\Filter;
+use Barryvdh\DomPDF\Facade\Pdf;
+use Carbon\Carbon;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Actions\Action;
 
 class MatriculaResource extends Resource
 {
     protected static ?string $model = Matricula::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
+
+    protected static ?string $navigationGroup = 'School';
+
+    protected static ?int $navigationSort = 0;
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Group::make()
+                    ->schema([
+                        Forms\Components\Section::make('Datos')
+                            ->schema([
+                                Forms\Components\TextInput::make('codigo')
+                                ->disabled()
+                                ->dehydrated(false)
+                                ->columnSpan('full')
+                                ->label('Código de Matricula')
+                                ->helperText('Este código se generará automáticamente al guardar.'),
+                                Forms\Components\TextInput::make('nombre')
+                                    ->required()
+                                    ->maxValue(50)
+                                    ->autocapitalize('words')
+                                    ->autocomplete(false),
+                                Forms\Components\TextInput::make('apellido')
+                                    ->required()
+                                    ->maxValue(50)
+                                    ->autocapitalize('words')
+                                    ->autocomplete(false),
+                                Forms\Components\TextInput::make('rut')
+                                    ->unique(
+                                        table: 'matriculas', 
+                                        ignoreRecord: true
+                                    )
+                                    ->required()
+                                    ->prefix('RUT')
+                                    ->maxValue(12)
+                                    ->autocomplete(false),
+                                Forms\Components\TextInput::make('correo')
+                                    ->prefixIcon('heroicon-m-envelope-open')
+                                    ->required()
+                                    ->email()
+                                    ->unique(ignoreRecord: true),
+                                Forms\Components\TextInput::make('telefono')
+                                    ->prefix('+56')
+                                    ->label('Telefono')
+                                    ->tel()
+                                    ->required()
+                                    ->telRegex('/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$/')
+                                    ->maxValue(20)
+                                    ->placeholder('977777777'),
+                                Forms\Components\TextInput::make('edad')
+                                    ->required()
+                                    ->maxValue(3),
+                                Forms\Components\TextInput::make('direccion')
+                                    ->required()
+                                    ->maxValue(255)
+                                    ->autocapitalize('words')
+                                    ->autocomplete(false),
+                                Forms\Components\TextInput::make('comuna')
+                                    ->required()
+                                    ->maxValue(50)
+                                    ->autocapitalize('words')
+                                    ->autocomplete(false),
+                                Forms\Components\Select::make('nivel')
+                                    ->label('Nivel de Escolaridad')
+                                    ->required()
+                                    ->options([
+                                        'Basica Incompleta' => 'Basica Incompleta',
+                                        'Basica Completa' => 'Basica Completa',
+                                        'Media Incompleta' => 'Media Incompleta',
+                                        'Media Completa' => 'Media Completa',
+                                        'Tecnico' => 'Tecnico',
+                                        'Universitario' => 'Universitario',
+                                    ]),
+                                Forms\Components\Select::make('licencia_actual')
+                                    ->required()
+                                    ->options([
+                                        'B' => 'B',
+                                        'A2' => 'A2',
+                                        'A3' => 'A3',
+                                        'A4' => 'A4',
+                                        'A5' => 'A5',
+                                        'A2-A4' => 'A2-A4',
+                                        'A3-A5' => 'A3-A5',
+                                    ]),    
+                                Forms\Components\Select::make('curso_id')
+                                    ->relationship('curso', 'codigo')
+                                    ->required()
+                                    ->searchable()
+                                    ->preload()
+                                    ->columnSpan('full'),
+                            ])->columns('2')
+                        ]),
+
+                        Forms\Components\Group::make()
+                            ->schema([
+                                Forms\Components\Section::make('Fechas')
+                                    ->schema([
+                                        Forms\Components\DatePicker::make('fecha_matricula')
+                                            ->date()
+                                            ->default(Carbon::now()->toDateString()),
+                                        Forms\Components\DatePicker::make('fecha_nacimiento')
+                                            ->date()
+                                            ->required(),
+                                        Forms\Components\FileUpload::make('image_url')
+                                            ->label('Sube una Imagen')
+                                            ->disk('public')
+                                            ->directory('image-filament')
+                                            ->image()
+                                            ->preserveFilenames()
+                                            ->imageEditor()
+                                            ->columnSpan('full')
+                                            
+                                    ])->columns('2')
+                            ])
             ]);
     }
 
@@ -31,17 +156,125 @@ class MatriculaResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\ImageColumn::make('image_url')
+                    ->label('Imagen')
+                    ->square(),
+                Tables\Columns\TextColumn::make('codigo')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nombre')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('apellido')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('rut')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('correo')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('telefono')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('edad')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('fecha_matricula')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('fecha_nacimiento')
+                    ->date()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('direccion')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('comuna')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nivel')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('licencia_actual')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'B' => 'primary',
+                        'A2' => 'warning',
+                        'A3' => 'success',
+                        'A4' => 'danger',
+                        'A5' => 'info',
+                    }),    
+                Tables\Columns\TextColumn::make('curso.codigo')
+                    ->label('Curso')
+                    ->sortable()
+                    ->toggleable(),
+                
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                Filter::make('fecha_matricula')
+                ->form([
+                    DatePicker::make('fecha_inicio'),
+                    DatePicker::make('fecha_termino'),
+                ])
+                ->query(function (Builder $query, array $data): Builder {
+                    return $query
+                        ->when(
+                            $data['fecha_inicio'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('fecha_matricula', '>=', $date),
+                        )
+                        ->when(
+                            $data['fecha_termino'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('fecha_matricula', '<=', $date),
+                        );
+                })
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+
+                // Boton seleccionar un contrato y poder imprimir un pdf del alumno
+                Action::make('imprimir_contrato')
+                ->label('Imprimir Contrato')
+                ->icon('heroicon-o-printer')
+                ->form([
+                    Select::make('tipo_contrato')
+                        ->label('Seleciona el Contrato')
+                        ->options([
+                            'matricula' => 'SIT',
+                            'prof' => 'PROF',
+                        ])
+                        ->required(),
+                ])
+                ->action(function (Matricula $record, array $data) {
+                    $tipoContrato = $data['tipo_contrato'];
+                    $view = $tipoContrato === 'matricula' ? 'matricula' : 'prof';
+                    
+                    $pdf = PDF::loadView("contratos.{$view}", ['matricula' => $record]);
+                    return response()->streamDownload(function () use ($pdf) {
+                        echo $pdf->output();
+                    }, "{$tipoContrato}_contrato.pdf");
+                }),
             ])
+
+            //->headerActions([
+            //    ExportAction::make()->exporter(MatriculaExporter::class)
+            //])
+            
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+
+
+                     // Boton para exportar en excel
+                ExportBulkAction::make()->exporter(MatriculaExporter::class)   
                 ]),
             ]);
     }
